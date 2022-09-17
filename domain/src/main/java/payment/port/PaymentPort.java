13d6641b54524186e157facfe8aff336becc508b
abbaszade <muradabbaszade6@gmail.com>
2022-09-17 22:00:58 +0400

    Delete domain/src/main/java/payment directory

--- domain/src/main/java/payment/port/PaymentPort.java
+++
@@ -1,8 +0,0 @@
-package payment.port;
-
-import payment.model.Payment;
-import payment.usecase.PaymentCreate;
-
-public interface PaymentPort {
-    Payment createPayment(PaymentCreate paymentCreate);
-}
